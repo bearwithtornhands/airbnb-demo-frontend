064421c753b715ceae652107ccae8cc50429e6c9
Alexandr Boev <alxndr.boev@gmail.com>
2018-01-31 13:20:16 +0300

    main/ homes data;

--- src/Homes/Catalog/index.js
+++ src/Homes/Catalog/index.js
@@ -2,39 +2,6 @@ import React from "react";
 import styled from "styled-components";
 import Card from "../Card";
 import Pagination from "../Pagination";
-// import homeImg1 from "./home-1.png";
-// import homeImg2 from "./home-2.png";
-// import homeImg3 from "./home-3.png";
-
-// const homesData = [
-//   {
-//     id: 1,
-//     url: "#url",
-//     image: homeImg1,
-//     title: "La Salentina, see, nature, & relax",
-//     descr: "Entrie house · 9 bed",
-//     price: 82,
-//     superhost: 97
-//   },
-//   {
-//     id: 2,
-//     url: "#url",
-//     image: homeImg2,
-//     title: "Yout private 3 bedr. riad and exclusive chanels in park",
-//     descr: "Entrie house · 5 bed",
-//     price: 55,
-//     superhost: 161
-//   },
-//   {
-//     id: 3,
-//     url: "#url",
-//     image: homeImg3,
-//     title: "Dreamy Tropical Tree House",
-//     descr: "Entrie house · 9 bed",
-//     price: 300,
-//     superhost: 364
-//   }
-// ];
 
 const Section = styled.div`
   padding-top: 81px;
--- src/Main/Homes/index.js
+++ src/Main/Homes/index.js
@@ -1,68 +1,71 @@
-import React from 'react';
-import { Section, Heading, HeadingH2, HeadingLink, SliderTrack, SliderRight } from '../../UI';
-import Card from '../../Homes/Card';
-import homeImg1 from './home-1.png';
-import homeImg2 from './home-2.png';
-import homeImg3 from './home-3.png';
+import React from "react";
+import {
+  Section,
+  Heading,
+  HeadingH2,
+  HeadingLink,
+  SliderTrack,
+  SliderRight
+} from "../../UI";
+import Card from "../../Homes/Card";
 
 const ButtonRight = SliderRight.extend`
   top: 102px;
 `;
 
-const homesData = [
-  {
-    id: 1,
-    url: '#url',
-    image: homeImg1,
-    title: 'La Salentina, see, nature, & relax',
-    descr: 'Entrie house · 9 bed',
-    price: 82,
-    superhost: 97,
-  },
-  {
-    id: 2,
-    url: '#url',
-    image: homeImg2,
-    title: 'Yout private 3 bedr. riad and exclusive chanels in park',
-    descr: 'Entrie house · 5 bed',
-    price: 55,
-    superhost: 161,
-  },
-  {
-    id: 3,
-    url: '#url',
-    image: homeImg3,
-    title: 'Dreamy Tropical Tree House',
-    descr: 'Entrie house · 9 bed',
-    price: 300,
-    superhost: 364,
-  },
-];
+export default class Homes extends React.Component {
+  state = {
+    data: []
+  };
 
-export default () => {
-  const list = homesData.map(home => (
-    <div key={home.id} className="col-xs-6 col-md-4">
-      <Card
-        url={home.url}
-        image={home.image}
-        title={home.title}
-        descr={home.descr}
-        price={home.price}
-        superhost={home.superhost}
-      />
-    </div>
-  ));
+  homesTemp = data => {
+    if (data.length) {
+      return data.map(item => (
+        <div key={item.id} className="col-xs-6 col-md-4">
+          <Card
+            url={"/"}
+            image={item.images[0].picture}
+            title={item.name}
+            descr={"descr"}
+            price={"price"}
+            isSuperhost={item.isSuperhost}
+            reviews={item.reviewsCount}
+          />
+        </div>
+      ));
+    }
 
-  return (
-    <Section>
-      <Heading>
-        <HeadingH2>Homes</HeadingH2>
-        <HeadingLink to="/homes">See all</HeadingLink>
-      </Heading>
-      <SliderTrack>
-        <div className="row">{list}</div>
-        <ButtonRight type="button" />
-      </SliderTrack>
-    </Section>
-  );
-};
+    return [];
+  };
+
+  componentDidMount() {
+    fetch("https://airbnb-demo-api.now.sh/v1/homes")
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Something went wrong ...");
+        }
+      })
+      .then(data => this.setState({ data: data.items }))
+      .catch(error => console.log(error));
+  }
+
+  render() {
+    const { data } = this.state;
+    const homesList = this.homesTemp(data);
+
+    return (
+      <Section>
+        <Heading>
+          <HeadingH2>Homes</HeadingH2>
+          <HeadingLink to="/homes">See all</HeadingLink>
+        </Heading>
+        <SliderTrack>
+          <div className="row">{homesList}</div>
+          <ButtonRight type="button" />
+        </SliderTrack>
+      </Section>
+    );
+  }
+}
